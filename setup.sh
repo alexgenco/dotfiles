c4bfa076818609f894c869a4aaf923061caf2115
Alex Genco <agenco@kabam.com>
2012-03-31 11:02:09 -0700

    setup script, need to test it out

---
+++ setup.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+if [ ! `dirname $0` == '.' ]; then
+  echo "Run $0 from inside its parent directory"
+  exit 1
+fi
+
+for dotfile in bash_profile gvimrc pryrc zshrc bash_aliases gitconfig vimrc vim oh-my-zsh
+do
+  if [ ! -f ./${dotfile} -a ! -d ./${dotfile} ]; then
+    echo "Couldn't find $dotfile in working directory. Did you rename it or delete it?"
+    exit 1
+  fi
+done
+
+for dotfile in bash_profile gvimrc pryrc zshrc bash_aliases gitconfig vimrc vim oh-my-zsh
+do
+  if [ -f ~/.$dotfile -o -d ~/.$dotfile ]; then
+    echo "Renaming ~/.$dotfile to ~/.${dotfile}_backup"
+    mv ~/.$dotfile ~/.${dotfile}_backup
+  fi
+
+  ln -s ./$dotfile ~/.$dotfile
+  echo "Linked ~/.$dotfile to $dotfile"
+done
+
+echo "Successfully setup dotfiles!"
+
+exit 0
