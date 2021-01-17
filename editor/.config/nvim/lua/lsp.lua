189b9081bc9d8cc9faff9d814bba3fefec1ed9b8
Alex Genco <alexgenco@gmail.com>
2021-01-17 12:02:11 -0800

    Refactor lua setup to use default config

--- editor/.config/nvim/lua/lsp.lua
+++ editor/.config/nvim/lua/lsp.lua
@@ -1,31 +1,38 @@
 local lsp = require'lspconfig'
+local util = lsp.util
 
-local on_attach = function(client, bufnr)
-  require'completion'.on_attach(client, bufnr)
+-- Set defaults for all servers
+util.default_config = vim.tbl_extend(
+  "force",
+  util.default_config, {
+    -- Default on_attach function for setup, creating mappings, etc.
+    on_attach = function(client, bufnr)
+      require'completion'.on_attach(client, bufnr)
 
-  require'lspfuzzy'.setup {
-    methods = {
-      'textDocument/codeAction',
-      'textDocument/documentSymbol',
-      'textDocument/references',
-    }
+      require'lspfuzzy'.setup {
+        methods = {
+          'textDocument/codeAction',
+          'textDocument/documentSymbol',
+          'textDocument/references',
+        }
+      }
+
+      local function map(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+      local opts = {noremap=true, silent=true}
+
+      map("",  '<leader>a',     '<cmd>lua vim.lsp.buf.code_action()<cr>',     opts)
+      map("n", '<leader><c-f>', '<cmd>lua vim.lsp.buf.formatting()<cr>',      opts)
+      map("n", '<leader>r',     '<cmd>lua vim.lsp.buf.references()<cr>',      opts)
+      map("n", '<leader>/',     '<cmd>lua vim.lsp.buf.document_symbol()<cr>', opts)
+      map("n", '<c-]>',         '<cmd>lua vim.lsp.buf.definition()<cr>',      opts)
+      map("n", '<c-k>',         '<cmd>lua vim.lsp.buf.hover()<cr>',           opts)
+    end
   }
+)
 
-  local function map(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local opts = {noremap=true, silent=true}
+lsp.gopls.setup{}
 
-  map("",  '<leader>a',     '<cmd>lua vim.lsp.buf.code_action()<cr>',     opts)
-  map("n", '<leader><c-f>', '<cmd>lua vim.lsp.buf.formatting()<cr>',      opts)
-  map("n", '<leader>r',     '<cmd>lua vim.lsp.buf.references()<cr>',      opts)
-  map("n", '<leader>/',     '<cmd>lua vim.lsp.buf.document_symbol()<cr>', opts)
-  map("n", '<c-]>',         '<cmd>lua vim.lsp.buf.definition()<cr>',      opts)
-  map("n", '<c-k>',         '<cmd>lua vim.lsp.buf.hover()<cr>',           opts)
-end
-
-lsp.gopls.setup{on_attach=on_attach}
-
-lsp.rust_analyzer.setup({
-  on_attach = on_attach,
+lsp.rust_analyzer.setup{
   settings = {
     ["rust-analyzer"] = {
       cargo = {
@@ -36,6 +43,6 @@ lsp.rust_analyzer.setup({
       }
     }
   }
-})
+}
 
 vim.g.loaded_lsp = 1
