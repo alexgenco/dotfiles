2b05386a3d06caeaa4470e2d9d4786260a0cbb62
Alex Genco <alexgenco@gmail.com>
2021-03-06 11:38:22 -0800

    LSP mappings

--- editor/.config/nvim/lua/lsp.lua
+++ editor/.config/nvim/lua/lsp.lua
@@ -8,23 +8,18 @@ util.default_config = vim.tbl_extend(
     -- Default on_attach function for setup, creating mappings, etc.
     on_attach = function(client, bufnr)
       require'completion'.on_attach(client, bufnr)
-
-      require'lspfuzzy'.setup {
-        methods = {
-          'textDocument/codeAction',
-          'textDocument/documentSymbol',
-        }
-      }
+      require'lspfuzzy'.setup{}
 
       local function map(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
       local opts = {noremap=true, silent=true}
 
-      map("",  '<leader>a',     '<cmd>lua vim.lsp.buf.code_action()<cr>',     opts)
-      map("n", '<leader><c-f>', '<cmd>lua vim.lsp.buf.formatting()<cr>',      opts)
-      map("n", '<leader>r',     '<cmd>lua vim.lsp.buf.references()<cr>',      opts)
-      map("n", '<leader>/',     '<cmd>lua vim.lsp.buf.document_symbol()<cr>', opts)
-      map("n", '<c-]>',         '<cmd>lua vim.lsp.buf.definition()<cr>',      opts)
-      map("n", '<c-k>',         '<cmd>lua vim.lsp.buf.hover()<cr>',           opts)
+      map("",  '<leader>a',     '<cmd>lua vim.lsp.buf.code_action()<cr>',        opts)
+      map("n", '<leader><c-f>', '<cmd>lua vim.lsp.buf.formatting()<cr>',         opts)
+      map("n", '<leader>r',     '<cmd>lua vim.lsp.buf.references()<cr>',         opts)
+      map("n", '<leader>/',     '<cmd>lua vim.lsp.buf.workspace_symbol("")<cr>', opts)
+      map("n", '<leader>d',     '<cmd>LspDiagnostics 0<cr>',                     opts)
+      map("n", '<c-]>',         '<cmd>lua vim.lsp.buf.definition()<cr>',         opts)
+      map("n", '<c-k>',         '<cmd>lua vim.lsp.buf.hover()<cr>',              opts)
     end
   }
 )
